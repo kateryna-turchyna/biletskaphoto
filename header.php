067363f0e510fabe587e363dacfe1320ff2c78b3
Igor Lazarev <igor@lazarevweb.com>
2017-07-23 19:56:27 +0100

    http to https

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 	<script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"></script>
-	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.js"></script>
 	<link rel = "stylesheet" href = "bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="main.css">
 </head>
