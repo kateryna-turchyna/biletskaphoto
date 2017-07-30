1efa40ef5991481deabeed5fb5283cb9fb751e81
Kateryna Turchyna <625807@gmail.com>
2017-07-30 20:17:30 +0100

    wedding

--- wedding.php
+++ wedding.php
@@ -11,41 +11,41 @@
 				<a href="#gallery" data-slide-to="0" class="grid__item grid__sizer"><img src="wedding/bride_and_flowers.jpg" data-toggle="modal" data-target="#myModal"></a>
 		   
 		 
-				<a href="#gallery" data-slide-to="1" class="grid__item" ><img src="wedding/bride and groom.jpg" data-toggle="modal" data-target="#myModal"></a>
+				<a href="#gallery" data-slide-to="1" class="grid__item" ><img src="wedding/bride_and_groom.jpg" data-toggle="modal" data-target="#myModal"></a>
 	   
-				<a href="#gallery" data-slide-to="2" class="grid__item"><img src="wedding/bride in the Chicago.jpg" data-toggle="modal" data-target="#myModal"></a>
+				<a href="#gallery" data-slide-to="2" class="grid__item"><img src="wedding/bride_in_the_Chicago.jpg" data-toggle="modal" data-target="#myModal"></a>
 		  
-				<a href="#gallery" data-slide-to="3" class="grid__item"><img src="wedding/bride moment.jpg" data-toggle="modal" data-target="#myModal"></a>
+				<a href="#gallery" data-slide-to="3" class="grid__item"><img src="wedding/bride_moment.jpg" data-toggle="modal" data-target="#myModal"></a>
 				
-				<a href="#gallery" data-slide-to="4" class="grid__item"><img src="wedding/bride style.jpg" data-toggle="modal" data-target="#myModal"></a>
+				<a href="#gallery" data-slide-to="4" class="grid__item"><img src="wedding/bride_style.jpg" data-toggle="modal" data-target="#myModal"></a>
 				
-				<a href="#gallery" data-slide-to="5" class="grid__item"><img src="wedding/couple at the lake.jpg" data-toggle="modal" data-target="#myModal"></a>
+				<a href="#gallery" data-slide-to="5" class="grid__item"><img src="wedding/couple_at_the_lake.jpg" data-toggle="modal" data-target="#myModal"></a>
 				
-				<a href="#gallery" data-slide-to="6" class="grid__item"><img src="wedding/couple at the street.jpg" data-toggle="modal" data-target="#myModal"></a>
+				<a href="#gallery" data-slide-to="6" class="grid__item"><img src="wedding/couple_at_the_street.jpg" data-toggle="modal" data-target="#myModal"></a>
 				
-				<a href="#gallery" data-slide-to="7" class="grid__item"><img src="wedding/couple hugging.jpg" data-toggle="modal" data-target="#myModal"></a>
+				<a href="#gallery" data-slide-to="7" class="grid__item"><img src="wedding/couple_hugging.jpg" data-toggle="modal" data-target="#myModal"></a>
 				
-				<a href="#gallery" data-slide-to="8" class="grid__item"><img src="wedding/happy couple.jpg" data-toggle="modal" data-target="#myModal"></a>
+				<a href="#gallery" data-slide-to="8" class="grid__item"><img src="wedding/happy_couple.jpg" data-toggle="modal" data-target="#myModal"></a>
 				
-				<a href="#gallery" data-slide-to="9" class="grid__item"><img src="wedding/her eyes.jpg" data-toggle="modal" data-target="#myModal"></a>
+				<a href="#gallery" data-slide-to="9" class="grid__item"><img src="wedding/her_eyes.jpg" data-toggle="modal" data-target="#myModal"></a>
 				
-				<a href="#gallery" data-slide-to="10" class="grid__item"><img src="wedding/on the motorcycle black and white.jpg" data-toggle="modal" data-target="#myModal"></a>
+				<a href="#gallery" data-slide-to="10" class="grid__item"><img src="wedding/on_the_motorcycle_black_and_white.jpg" data-toggle="modal" data-target="#myModal"></a>
 				
-				<a href="#gallery" data-slide-to="11" class="grid__item"><img src="wedding/wedding details.jpg" data-toggle="modal" data-target="#myModal"></a>
+				<a href="#gallery" data-slide-to="11" class="grid__item"><img src="wedding/wedding_details.jpg" data-toggle="modal" data-target="#myModal"></a>
 				
-				<a href="#gallery" data-slide-to="12" class="grid__item"><img src="wedding/wedding in chicago.jpg" data-toggle="modal" data-target="#myModal"></a>
+				<a href="#gallery" data-slide-to="12" class="grid__item"><img src="wedding/wedding_in_chicago.jpg" data-toggle="modal" data-target="#myModal"></a>
 				
-				<a href="#gallery" data-slide-to="13" class="grid__item"><img src="wedding/wedding shoot.jpg" data-toggle="modal" data-target="#myModal"></a>
+				<a href="#gallery" data-slide-to="13" class="grid__item"><img src="wedding/wedding_shoot.jpg" data-toggle="modal" data-target="#myModal"></a>
 
-				<a href="#gallery" data-slide-to="14" class="grid__item"><img src="wedding/bride with roses.jpg" data-toggle="modal" data-target="#myModal"></a>
+				<a href="#gallery" data-slide-to="14" class="grid__item"><img src="wedding/bride_with_roses.jpg" data-toggle="modal" data-target="#myModal"></a>
 
-				<a href="#gallery" data-slide-to="15" class="grid__item"><img src="wedding/couple and bridge.jpg" data-toggle="modal" data-target="#myModal"></a>
+				<a href="#gallery" data-slide-to="15" class="grid__item"><img src="wedding/couple_and_bridge.jpg" data-toggle="modal" data-target="#myModal"></a>
 
-				<a href="#gallery" data-slide-to="16" class="grid__item"><img src="wedding/Untitled-2.jpg" data-toggle="modal" data-target="#myModal"></a>
+				<a href="#gallery" data-slide-to="16" class="grid__item"><img src="wedding/pink_roses.jpg" data-toggle="modal" data-target="#myModal"></a>
 				
-				<a href="#gallery" data-slide-to="17" class="grid__item"><img src="wedding/wedding in lincoln park.jpg" data-toggle="modal" data-target="#myModal"></a>
+				<a href="#gallery" data-slide-to="17" class="grid__item"><img src="wedding/wedding_in_lincoln_park.jpg" data-toggle="modal" data-target="#myModal"></a>
 				
-				<a href="#gallery" data-slide-to="18" class="grid__item"><img src="wedding/hands close up.jpg" data-toggle="modal" data-target="#myModal"></a>
+				<a href="#gallery" data-slide-to="18" class="grid__item"><img src="wedding/hands_close_up.jpg" data-toggle="modal" data-target="#myModal"></a>
 	   
 
 			
@@ -54,7 +54,6 @@
 		<div class="modal-dialog">
 			<div class="modal-content center-block">
 				<div class="modal-header">
-					<div class="pull-left">Wedding/Couples</div>
 						<button type="button" class="close" data-dismiss="modal" title="Close"> <span class="glyphicon glyphicon-remove"></span></button>
 				</div>
 
@@ -63,67 +62,61 @@
 				<div id="gallery" class="carousel slide carousel-fade center-block" data-ride="carousel" data-interval="false">
 				<div class="carousel-inner center-block">
 				
-					<div class="item active vertical_modal center-block"><img src="wedding/bride and flowers.jpg" alt="item0">
+					<div class="item active vertical_modal center-block"><img src="wedding/bride_and_flowers.jpg" alt="item0">
 					</div>
 					
-					<div class="item vertical_modal center-block"> <img src="wedding/bride and groom.jpg" alt="item1">
+					<div class="item vertical_modal center-block"> <img src="wedding/bride_and_groom.jpg" alt="item1">
 					</div>
 					
-					<div class="item vertical_modal center-block"> <img src="wedding/bride in the Chicago.jpg" alt="item2">
+					<div class="item vertical_modal center-block"> <img src="wedding/bride_in_the_Chicago.jpg" alt="item2">
 					</div>
 					
-					<div class="item horizontal_modal center-block"> <img src="wedding/bride moment.jpg" alt="item3">
+					<div class="item horizontal_modal center-block"> <img src="wedding/bride_moment.jpg" alt="item3">
 					</div>
 					
-					<div class="item vertical_modal center-block"><img src="wedding/bride style.jpg" alt="item4">
+					<div class="item vertical_modal center-block"><img src="wedding/bride_style.jpg" alt="item4">
 					</div>
 					
-					<div class="item horizontal_modal center-block"><img src="wedding/couple at the lake.jpg" alt="item0">
+					<div class="item horizontal_modal center-block"><img src="wedding/couple_at_the_lake.jpg" alt="item0">
 					</div>
 					
-					<div class="item horizontal_modal center-block"><img src="wedding/couple at the street.jpg" alt="item0">
+					<div class="item horizontal_modal center-block"><img src="wedding/couple_at_the_street.jpg" alt="item0">
 					</div>
 					
-					<div class="item horizontal_modal center-block"><img src="wedding/couple hugging.jpg" alt="item0">
+					<div class="item horizontal_modal center-block"><img src="wedding/couple_hugging.jpg" alt="item0">
 					</div>
 					
-					<div class="item horizontal_modal center-block"><img src="wedding/happy couple.jpg" alt="wedding in lincoln park">
+					<div class="item horizontal_modal center-block"><img src="wedding/happy_couple.jpg" alt="wedding in lincoln park">
 					</div>
 					
-					<div class="item horizontal_modal center-block"><img src="wedding/her eyes.jpg" alt="item0">
+					<div class="item horizontal_modal center-block"><img src="wedding/her_eyes.jpg" alt="item0">
 					</div>
 					
-					<div class="item horizontal_modal center-block"><img src="wedding/on the motorcycle black and white.jpg" alt="item0">
+					<div class="item horizontal_modal center-block"><img src="wedding/on_the_motorcycle_black_and_white.jpg" alt="item0">
 					</div>
 					
-					<div class="item sq_modal center-block"><img src="wedding/wedding details.jpg" alt="item0">
+					<div class="item sq_modal center-block"><img src="wedding/wedding_details.jpg" alt="item0">
 					</div>
 					
-					<div class="item horizontal_modal center-block"><img src="wedding/wedding in chicago.jpg" alt="item0">
+					<div class="item horizontal_modal center-block"><img src="wedding/wedding_in_chicago.jpg" alt="item0">
 					</div>
 					
-					<div class="item vertical_modal center-block"><img src="wedding/wedding shoot.jpg" alt="item0">
+					<div class="item vertical_modal center-block"><img src="wedding/wedding_shoot.jpg" alt="item0">
 					</div>
 
-					<div class="item horizontal_modal center-block"><img src="wedding/bride in Chicago.jpg" alt="item0">
+					<div class="item vertical_modal center-block"><img src="wedding/bride_with_roses.jpg" alt="item0">
 					</div>
 
-					<div class="item vertical_modal center-block"><img src="wedding/bride with roses.jpg" alt="item0">
+					<div class="item vertical_modal center-block"><img src="wedding/couple_and_bridge.jpg" alt="item0">
 					</div>
 
-					<div class="item vertical_modal center-block"><img src="wedding/couple and bridge.jpg" alt="item0">
-					</div>
-
-					<div class="item horizontal_modal center-block"><img src="wedding/hands of couple.jpg" alt="item0">
-					</div>
-
-					<div class="item horizontal_modal center-block"><img src="wedding/Untitled-2.jpg" alt="item0">
+					<div class="item horizontal_modal center-block"><img src="wedding/pink_roses.jpg" alt="item0">
 					</div>
 					
-					<div class="item horizontal_modal center-block"><img src="wedding/wedding in lincoln park.jpg" alt="wedding in lincoln park">
+					<div class="item horizontal_modal center-block"><img src="wedding/wedding_in_lincoln_park.jpg" alt="wedding in lincoln park">
 					</div>
 					
-					<div class="item horizontal_modal center-block"><img src="wedding/hands close up.jpg" alt="item0">
+					<div class="item horizontal_modal center-block"><img src="wedding/hands_close_up.jpg" alt="item0">
 					</div>
 				<!--end carousel-inner-->
 				</div>
@@ -135,9 +128,6 @@
 			</div>
 			
 			<div class="modal-footer">
-			<div class="pull-left">
-			<small>Pictures by Jhanna Biletska</small>
-			</div>
 			<button class="btn-sm close" type="button" data-dismiss="modal">Close</button>
 			<!--end modal-footer-->
 			</div>
